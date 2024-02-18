e642cbf960d2a9ec2406cbe03b00c13dbc81eebb
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-17 20:53:15 -0500

    Add: configuration postgres

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
 
-
 ## STACK
-
 - NestJs
 - TypeScript
 - PostgreSQL
 - TypeORM
+
+## Teslo API
+1. Clonar el proyecto
+2. ```npm install```
+3. Clonar el archivo ```.env.example``` y renombrarlo a ```.env```
+4. Cambiar las variables de entorno
+5. Levantar la base de datos
+```
+docker-compose.yml
+```
+6. Levantar el proyecto: ```npm run start:dev```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(), // configurar variables de entorno
+    TypeOrmModule.forRoot({ // configurar type orm
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, // cargar automaticamente las entidades
+      synchronize: true,
+    }), 
+  ],
 })
 export class AppModule {}
