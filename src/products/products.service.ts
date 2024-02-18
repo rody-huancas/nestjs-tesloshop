4cb5acb40ced385257955f32509d24c6af81fb92
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-18 00:39:59 -0500

    Update: getBy product by id, slug and title

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID} from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -34,10 +35,19 @@ export class ProductsService {
     })
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product;
+
+    if( isUUID(term) ) {
+      product = await this.productRepository.findOneBy({ id: term });
+    }else{
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('UPPER(title) = :title or slug = :slug', { title: term.toUpperCase(), slug: term.toLocaleLowerCase() }).getOne();
+    }
+
     if( !product )
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with ${term} not found`);
     return product;
   }
 
