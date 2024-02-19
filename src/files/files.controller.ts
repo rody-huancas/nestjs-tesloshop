37abdd65039583e76175c8c1ab612c26df484ef2
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-19 02:25:59 -0500

    Add: validation type image

---
+++ src/files/files.controller.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter 
+  }) )
+  uploadProductFile( @UploadedFile() file: Express.Multer.File ){
+    if( !file ) {
+      throw new BadRequestException('Make sure that the file is an image')
+    }
+
+    return {
+      filename: file.originalname
+    }
+  }
+
+}
